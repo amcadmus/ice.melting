e412e3a60a71e552ad79ffd6d1e1fae4a0c00f40
amcadmus <amcadmus@gmail.com>
2015-04-02 20:00:42 +0200

    backup changes

--- templates/template.std.water/run.sh
+++ templates/template.std.water/run.sh
@@ -7,8 +7,11 @@ sed -e "s/ref_t.*=.*/ref_t = $temperature/g" grompp.mdp | \
 sed -e "s/nsteps.*=.*/nsteps = $nsteps/g" > tmp.tmp
 mv -f tmp.tmp grompp.mdp
 
-grompp_mpi
-mpirun -n 4 mdrun_mpi  -v 
-echo 3 | trjconv_mpi -f traj.xtc -o oxygen.xtc -n index.ndx -b 50
+echo "# using `which grompp`"
+grompp
+echo "# using `which mdrun`"
+mdrun  -v 
+echo 3 | trjconv -f traj.xtc -o oxygen.xtc -n index.ndx -b 50
+echo "# using `which mpirun` and `which plumed`"
 mpirun -n 4 plumed driver --mf_xtc oxygen.xtc --plumed plumed.input
 
