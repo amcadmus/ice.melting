46d24b0fb7125e00c395d263e98030a0099819ce
amcadmus <amcadmus@gmail.com>
2015-04-02 22:56:54 +0200

    fix bugs

--- templates/template.remd/equi.sh
+++ templates/template.remd/equi.sh
@@ -9,9 +9,35 @@ gmx_nsteps=`echo "($gmx_dt*0.5+$gmx_equi_time) / $gmx_dt" | bc`
 
 nproc=`echo $temperatures | wc -w`
 
+for temperature in $temperatures ;
+do
+    dir_current=dir.$temperature
+    if test ! -d $dir_current; then
+	echo "does not exist dir $dir_current, exit"
+	exit
+    fi
+    echo "# grompp in dir $dir_current"
+    cd $dir_current
+    $grompp_cmd -n index.ndx
+    cd $dir_base
+done
+
 command="mpirun -n $nproc $mdrun_cmd -multidir dir* -v -nsteps $gmx_nsteps"
 echo "#run with command: $command"
 $command
+
+for temperature in $temperatures ;
+do
+    dir_current=dir.$temperature
+    if test ! -d $dir_current; then
+	echo "does not exist dir $dir_current, exit"
+	exit
+    fi
+    echo "# update config $dir_current"
+    cd $dir_current
+    mv -f confout.gro conf.gro
+    cd $dir_base
+done
 
 # for temperature in $temperatures ;
 # do 
--- templates/template.remd/run.sh
+++ templates/template.remd/run.sh
@@ -10,6 +10,19 @@ gmx_nreplex=`echo "($gmx_dt*0.5+$gmx_replex) / $gmx_dt" | bc`
 
 nproc=`echo $temperatures | wc -w`
 
+for temperature in $temperatures ;
+do
+    dir_current=dir.$temperature
+    if test ! -d $dir_current; then
+	echo "does not exist dir $dir_current, exit"
+	exit
+    fi
+    echo "# grompp in dir $dir_current"
+    cd $dir_current
+    $grompp_cmd -n index.ndx
+    cd $dir_base
+done
+
 command="mpirun -n $nproc $mdrun_cmd -multidir dir* -replex $gmx_nreplex -v -nsteps $gmx_nsteps"
 echo "#run with command: $command"
 $command
