5f082ab0c6b6a6e2653f8456a5fe219075a345b1
amcadmus <amcadmus@gmail.com>
2015-04-02 22:31:50 +0200

    better sh

--- templates/template.remd/equi.sh
+++ templates/template.remd/equi.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 source parameters.sh
+source env.sh
+source $GMX_DIR/bin/GMXRC
 
 dir_base=`pwd`
 gmx_nsteps=`echo "($gmx_dt*0.5+$gmx_equi_time) / $gmx_dt" | bc`
 
-for temperature in $temperatures ;
-do 
-    dir_current=dir.$temperature
-    if test ! -d $dir_current; then
-	echo "does not exist dir $dir_current, exit"
-	exit
-    fi
-    
-    echo "# run in dir $dir_current"
-    cd $dir_current
-    $grompp_cmd -n index.ndx
-    $mdrun_equi_cmd -nsteps $gmx_nsteps
-    cd $dir_base
-done
+nproc=`echo $temperatures | wc -w`
+
+command="mpirun -n $nproc $mdrun_cmd -multidir dir* -v -nsteps $gmx_nsteps"
+echo "#run with command: $command"
+$command
+
+# for temperature in $temperatures ;
+# do 
+#     dir_current=dir.$temperature
+#     if test ! -d $dir_current; then
+# 	echo "does not exist dir $dir_current, exit"
+# 	exit
+#     fi    
+#     echo "# run in dir $dir_current"
+#     cd $dir_current
+#     $grompp_cmd -n index.ndx
+#     $mdrun_equi_cmd -nsteps $gmx_nsteps
+#     mv -f confout.gro conf.gro
+#     cd $dir_base
+# done
     
--- templates/template.remd/run.sh
+++ templates/template.remd/run.sh
@@ -2,16 +2,15 @@
 
 source parameters.sh
 source env.sh
-
 source $GMX_DIR/bin/GMXRC
 
 dir_base=`pwd`
 gmx_nsteps=`echo "($gmx_dt*0.5+$gmx_time) / $gmx_dt" | bc`
 gmx_nreplex=`echo "($gmx_dt*0.5+$gmx_replex) / $gmx_dt" | bc`
 
-nrep=`echo $temperatures | wc -w`
+nproc=`echo $temperatures | wc -w`
 
-command="mpirun -n $nrep $mdrun_cmd -multidir dir* -replex $gmx_nreplex -v -nsteps $gmx_nsteps"
+command="mpirun -n $nproc $mdrun_cmd -multidir dir* -replex $gmx_nreplex -v -nsteps $gmx_nsteps"
 echo "#run with command: $command"
 $command
 
