b47dbc84dd313e954f81f5d0f53ce46b4b87b2d7
amcadmus <amcadmus@gmail.com>
2015-04-24 15:39:39 +0200

    add afed dir for q4 q6 layer z simulation

---
+++ templates/template.afed.q4q6lz/env.x220.sh
@@ -0,0 +1,1 @@
+GMX_DIR=/home/wanghan/local/gromacs/4.6.7
---
+++ templates/template.afed.q4q6lz/parameters.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# system setting
+system_box="1 1 1"
+system_seed_dir=md.seed
+system_base_conf=conf.gro	# in $system_seed_dir
+system_running_dir=running
+
+# run time control
+gmx_time=1000000		# in ps
+gmx_dt=0.002			# in ps
+gmx_temperature=252.10		# in K
+gmx_tau_t=0.1			# in ps
+gmx_tau_box=40			# in ps
+gmx_pcoupltype=anisotropic
+plumed_temperature=200000	# in K
+plumed_q4_tau=1000		# in ps
+plumed_q4_kappa=500000		# take 432 water system as reference
+plumed_q6_tau=1000		# in ps
+plumed_q6_kappa=500000		# take 432 water system as reference
+plumed_zl_tau=1000		# in ps
+plumed_zl_kappa=500000		# take 432 water system as reference
+plumed_zl_ref_numb=6
+plumed_nlist_rc=0.7
+plumed_nlist_feq=50
+
+# output control
+gmx_xtc_feq=1.0			# in ps
+gmx_trr_feq=0			# in ps
+gmx_energy_feq=1.0		# in ps
+
+# commands
+grompp_cmd=grompp_mpi
+mdrun_cmd=mdrun_mpi
+#gmx_dir=$HOME/local/gromacs/4.6.5
+
+
