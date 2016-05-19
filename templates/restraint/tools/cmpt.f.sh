b772a55367bf3d1caf520f0710f2c5cada93dde6
Han Wang <wang_han@iapcm.ac.cn>
2016-05-19 15:27:54 +0800

    add cmd line parameter for tools

--- templates/restraint/tools/cmpt.f.sh
+++ templates/restraint/tools/cmpt.f.sh
@@ -3,19 +3,43 @@
 source parameters.sh
 source env.sh
 
+target_dir="."
+if [ $# -ge 1 ]; then
+    target_dir=$1
+fi
+if [ ! -d $target_dir ]; then
+    echo "no target $target_dir, exit"
+    exit
+fi
+
 tools_dir=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 cwd=`pwd`
+cd $target_dir
 
-numb_res=`grep HarmonicRestraint $top_file | wc -l`
+numb_res=`grep HarmonicRestraint top.input | wc -l`
 numb_centers=`echo $res_centers | awk -F ',' '{print NF}'`
 if test $numb_centers -ne $numb_res; then
     echo "find $numb_res in top while find $numb_centers restraint center, inconsistency, do nothing."
     exit
 fi
-    
+
+line=""
 for ii in `seq 1 $numb_res`
 do
-    key=`grep HarmonicRestraint top.input | head -n $ii | tail -n 1 | awk '{print $1}'`
+    param_line=`grep HarmonicRestraint top.input  | head -n $ii | tail -n 1`
+    key=`echo $param_line | sed 's/.*input =\(.*\)k.*/\1/g' | awk -F'"' '{print $2}'`
+    center=`echo $param_line | sed 's/.*center =\(.*\)bias.*/\1/g'`
+    kk=`echo $param_line | sed 's/.*k =\(.*\)center.*/\1/g'`
     col=`grep Print top.input | sed 's/.*input =\(.*\)file.*/\1/g' | sed 's/\"//g' | sed 's/,/\n/g' | grep -n $key | awk -F':' '{print $1}'`
-    echo $key $col
+    col=`echo "$col+1" | bc`   
+    echo "# $key $col $center $kk"
+    awk "{print - (\$$col - $center) * $kk}" mace.out > tmp.out
+    force=`$moasp_bin_dir/$moasp_avg -f tmp.out -t .9 | grep -v \#  | awk '{print $1}'`
+    if [ $? -ne 0 ]; then echo "unsuccessful msp_avg, exit"; exit; fi
+    line="$line $force"
 done
+rm -f tmp.out
+
+cd $cwd
+echo $line
+
--- templates/restraint/tools/last.conf.sh
+++ templates/restraint/tools/last.conf.sh
@@ -3,13 +3,23 @@
 source env.sh
 source $gmx_bin_dir/GMXRC.bash
 
-if [ ! -d viz ]; then
-    echo "no viz, exit"
+target_dir="."
+if [ $# -ge 1 ]; then
+    target_dir=$1
+fi
+if [ ! -d $target_dir ]; then
+    echo "no target $target_dir, exit"
+    exit
+fi
+viz_dir=$target_dir/viz
+if [ ! -d $viz_dir ]; then
+    echo "no viz $viz_dir, exit"
     exit
 fi
 
-frame_id=`tail viz/REAL_SPACE/dumps.javis -n 1 | cut -d '/' -f 1 | cut -d '.' -f 2`
+frame_id=`tail $viz_dir/REAL_SPACE/dumps.javis -n 1 | cut -d '/' -f 1 | cut -d '.' -f 2`
 
+cd $target_dir
 $moasp_bin_dir/$moasp_ctj -n $frame_id -m trr
 if [ $? -ne 0 ]; then echo "unsuccessful msp_cvt_traj, exit"; exit; fi
 
@@ -21,4 +31,3 @@ echo 0 | $gmx_trjconf -f ../out.trr -o ../out.gro -pbc mol &> /dev/null
 if [ $? -ne 0 ]; then echo "unsuccessful trajconv, exit"; exit; fi
 
 cd ..
-
