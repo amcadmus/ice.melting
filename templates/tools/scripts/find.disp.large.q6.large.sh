4c68617779e89204b16af5ca5372bb8246217efa
Han Wang <han.wang.caep@gmail.com>
2017-02-05 00:41:50 +0800

    add scripts

---
+++ templates/tools/scripts/find.disp.large.q6.large.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+tq6=0.324
+tdispl=0.4
+
+echo "# index q6 displ"
+
+for ii in `seq 0 3455`
+do 
+    pii=`printf %06d $ii`
+    q6=`tail -n 1 steinhardt/mol_$pii | awk '{print $3}'`
+    displ=`tail -n 1 displ/mol_$pii | awk '{print $3}'`
+#    echo $ii $q6 $displ
+    if [ `echo "$q6 > $tq6" | bc -l` -eq 1 ] && [ `echo "$displ > $tdispl" | bc -l` -eq 1 ]; then
+	echo $ii $q6 $displ
+    fi
+done
+
