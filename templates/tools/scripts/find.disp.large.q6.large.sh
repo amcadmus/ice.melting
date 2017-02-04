8faf990b1d064948fcd29d97e8f7e6f6af8a066e
Han Wang <han.wang.caep@gmail.com>
2017-02-05 01:11:22 +0800

    add command line parameters for the thresholds

--- templates/tools/scripts/find.disp.large.q6.large.sh
+++ templates/tools/scripts/find.disp.large.q6.large.sh
@@ -3,6 +3,20 @@
 tq6=0.324
 tdispl=0.4
 
+echo "# usage "
+echo "# $0 [threshold_q6] [threshold_displ]"
+
+if test $# -ge 1; then
+    tq6=$1
+fi
+
+if test $# -ge 2; then
+    tdispl=$2
+fi
+
+echo "# threshold_q6:	 $tq6"
+echo "# threshold_dipl:	 $tdispl"
+
 echo "# index q6 displ"
 
 for ii in `seq 0 3455`
