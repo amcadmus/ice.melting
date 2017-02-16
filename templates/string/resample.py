377ce001bdd0a0319c4c44ac13b7776c0c95cecb
Han Wang <han.wang.caep@gmail.com>
2017-02-16 14:51:37 -0500

    revised resample test

--- templates/string/resample.py
+++ templates/string/resample.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     weight = np.loadtxt ("weight.out")
     print (string.shape[0] + 10)
     new_string = string_utils.resample_string (string, string.shape[0] + 16, weight)
-    new1_string = string_utils.resample_string (new_string, new_string.shape[0])
+    new1_string = string_utils.resample_string (new_string, new_string.shape[0], weight)
 #    new1_string = resample_string (new_string, weight, 1 + 2 * (new_string.shape[0] - 1))
     np.savetxt ("new.out", new_string)
     np.savetxt ("new1.out", new1_string)
