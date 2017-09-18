00ab0cc13408e0e8edf62efab006e52a4b692d46
Han Wang <han.wang.caep@gmail.com>
2017-09-18 13:57:32 +0800

    compute local vol in defect ana

--- templates/tools/scripts/do.defect.sh
+++ templates/tools/scripts/do.defect.sh
@@ -15,9 +15,12 @@ do
     fi
 #    /home/wanghan/study/ice.melting/templates/tools/h.bond/hbond  --detail --mol-defect		-f gmx.tool/pbc.xtc -t 2
     /home/wanghan/study/ice.melting/templates/tools/cv/steinhardt --detail --mol-value  -a 	-f gmx.tool/pbc.xtc -t 2
+    /home/wanghan/study/ice.melting/templates/tools/cv/locvol     --detail --mol-value  -a 	-f gmx.tool/pbc.xtc -t 2
 #    /home/wanghan/study/ice.melting/templates/tools/scripts/mole.acorr.sh 	nhbond 1000
     /home/wanghan/study/ice.melting/templates/tools/scripts/mole.smth.traj.sh 	steinhardt steinhardt.avg 
     /home/wanghan/study/ice.melting/templates/tools/scripts/mole.to.time.py 	steinhardt.avg 
+    /home/wanghan/study/ice.melting/templates/tools/scripts/mole.smth.traj.sh 	locvol locvol.avg 
+    /home/wanghan/study/ice.melting/templates/tools/scripts/mole.to.time.py 	locvol.avg 
     /home/wanghan/study/ice.melting/templates/tools/scripts/cv.defect.sh 	-f gmx.tool/pbc.xtc -q steinhardt.avg -t1 .324 -t2 .274
 #    /home/wanghan/study/ice.melting/templates/tools/scripts/cv.crystal.sh 	-f gmx.tool/pbc.xtc -q steinhardt.avg -t1 .324 -t2 .274
 #    /home/wanghan/study/ice.melting/templates/tools/scripts/plot.script.sh	steinhardt.avg.def.tcl figs.qdef/
